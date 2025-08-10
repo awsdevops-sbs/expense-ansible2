d96c196b15ec499ab1997b8138682d4bc8b2604c
akram <akram.khan200@gmail.com>
2025-08-10 23:44:44 +0530

    Pipelines example new machines

--- ansible.sh
+++ ansible.sh
@@ -1,6 +1,6 @@
 component=$1
 
-ansible-playbook -i "${component}.awsdevops.sbs," \
+ansible-playbook -i "${component}-${dev}.awsdevops.sbs," \
   -e "ansible_user=ec2-user" \
   -e "ansible_password=DevOps321" \
   -e "role_name=${component}" \
