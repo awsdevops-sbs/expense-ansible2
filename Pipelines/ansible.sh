5500380e015fe5f01790f56f028f44f809a3463f
akram <akram.khan200@gmail.com>
2025-08-10 12:26:13 +0530

    Pipelines example

--- Pipelines/ansible.sh
+++ Pipelines/ansible.sh
@@ -1,4 +1,4 @@
 component=$1
 
 
-ansible-playbook -i {component}.awsdevops.sbs, -e ansible_username=ec2-user -e ansible_password=DevOps321 expense.yml -e role_name={component} -e env=dev
+ansible-playbook -i component.awsdevops.sbs, -e ansible_username=ec2-user -e ansible_password=DevOps321 expense.yml -e role_name=component -e env=dev
